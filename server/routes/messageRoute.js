8798c91c35a1efd510ac1b3ce6ae5d2d32c217f5
Fakevin0613 <ke_qinyang@outlook.com>
2022-09-07 10:20:04 -0400

    move server to other dir

--- server/routes/messageRoute.js
+++
@@ -1,9 +0,0 @@
-const { addMessage, getAllMessage } = require('../controller/messageController');
-
-
-const router = require('express').Router();
-
-router.post("/addMessage", addMessage)
-router.post("/getAllMessage", getAllMessage)
-
-module.exports = router;
